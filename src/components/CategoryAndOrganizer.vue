53b3b720c553946720f9e2defabb805aef863d04
Decha_L <blackneko1234@hotmail.com>
2021-07-15 14:27:24 +0700

    3rd
    3rd

---
+++ src/components/CategoryAndOrganizer.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="event-card">
+    <div>Category: {{ categoryAndorganizer.category }}</div>
+    <div>Organizer Name:{{ categoryAndorganizer.organizer }}</div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'CategoryAndOrganizer',
+  props: {
+    categoryAndorganizer: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.event-card {
+  padding: 20px;
+  width: 250px;
+  height: 120px;
+  cursor: pointer;
+  border: 1px solid blue;
+  margin-bottom: 18px;
+  background-color: green;
+  color: rgb(179, 255, 0);
+  text-align: end;
+}
+
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,90 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="events">
+    <tr>
+      <td>
+        <EventCard v-for="event in events" :key="event.id" :event="event" />
+      </td>
+
+      <td>
+        <CategoryAndOrganizer
+          v-for="event in events"
+          :key="event.id"
+          :categoryAndorganizer="event"
+        />
+      </td>
+    </tr>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import EventCard from '@/components/EventCard.vue'
+import CategoryAndOrganizer from '@/components/CategoryAndOrganizer.vue'
 
 export default {
-  name: "Home",
+  name: 'Home',
   components: {
-    HelloWorld,
+    EventCard,
+    CategoryAndOrganizer
   },
-};
+  data() {
+    return {
+      events: [
+        {
+          id: 5928101,
+          category: 'animal welfare',
+          title: 'Cat Adoption Day',
+          description: 'Find your new feline friend at this event.',
+          location: 'Meow Town',
+          date: 'January 28, 2022',
+          time: '12:00',
+          petsAllowed: true,
+          organizer: 'Kat Laydee'
+        },
+        {
+          id: 4582797,
+          category: 'food',
+          title: 'Community Gardening',
+          description: 'Join us as we tend to the community edible plants.',
+          location: 'Flora City',
+          date: 'March 14, 2022',
+          time: '10:00',
+          petsAllowed: true,
+          organizer: 'Fern Pollin'
+        },
+        {
+          id: 8419988,
+          category: 'sustainability',
+          title: 'Beach Cleanup',
+          description: 'Help pick up trash along the shore.',
+          location: 'Playa Del Carmen',
+          date: 'July 22, 2022',
+          time: '11:00',
+          petsAllowed: false,
+          organizer: 'Carey Wales'
+        }
+      ]
+    }
+  }
+}
 </script>
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h4 {
+  font-size: 20px;
+}
+</style>
